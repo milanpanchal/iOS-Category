318fc8f823c7d914df8460d8e5896ab864588676
Milan Panchal <sam07it22@gmail.com>
2014-09-11 15:13:37 +0530

    Base64 and UIViewSize shortcuts added

--- UIView+Additional.h
+++ UIView+Additional.h
@@ -18,4 +18,31 @@
 
 - (void)fadeIn ;
 
+#pragma mark - Size shortcuts
+
+@property (nonatomic) CGFloat left;
+
+@property (nonatomic) CGFloat top;
+
+@property (nonatomic) CGFloat right;
+
+@property (nonatomic) CGFloat bottom;
+
+@property (nonatomic) CGFloat width;
+
+@property (nonatomic) CGFloat height;
+
+@property (nonatomic) CGFloat centerX;
+
+@property (nonatomic) CGFloat centerY;
+
+@property (nonatomic) CGPoint origin;
+
+@property (nonatomic) CGSize size;
+
+/**
+ * Return the coordinate on the screen.
+ */
+- (CGPoint)clientToScreen:(CGPoint)pt;
+
 @end
